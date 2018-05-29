08785362a61d4896472700c04f8386601d0c8942
stoklosaszymon <stoklosa.szymon@gmail.com>
2018-05-29 20:48:41 +0200

    player class added

--- agar.js
+++ agar.js
@@ -1,6 +1,6 @@
 var food = [];
 var zoom = 1;
-var player = [];
+
 
 class Blob {
   constructor(x, y, radius) {
@@ -20,7 +20,7 @@ class Blob {
 
 }
 
-class Player extends Blob {
+class PlayerBlob extends Blob {
 
   constructor(x, y, r) {
     super(x, y, r);
@@ -29,7 +29,7 @@ class Player extends Blob {
 
   updatePos() {
     var newvel = createVector(mouseX - width / 2, mouseY - height / 2);
-    newvel.setMag(3);
+    newvel.setMag( 500/this.calculateRadius());
     this.vel.lerp(newvel, 0.1);
     this.pos.add(this.vel);
   }
@@ -52,12 +52,12 @@ class Player extends Blob {
 
   combine(other) {
     var rad = 0;
-    for (var i = 0; i < player.length; i++) {
-      rad += player[i].calculateRadius();
+    for (var i = 0; i < player.blobs.length; i++) {
+      rad += player.blobs[i].calculateRadius();
     }
-    player[0].r = rad
+    player.blobs[0].r = rad
 
-    player.splice(1, player.length - 1);
+    player.blobs.splice(1, player.blobs.length - 1);
   }
 
   eats(other) {
@@ -72,22 +72,29 @@ class Player extends Blob {
   }
 
   static split() {
-    for (var i = player.length - 1; i >= 0; i--) {
-      if (player[i].r > 50) {
-        player[i].r /= 2;
+    for (var i = player.blobs.length - 1; i >= 0; i--) {
+      if (player.blobs[i].r > 50) {
+        player.blobs[i].r /= 2;
         var _vel = createVector(mouseX - width / 2, mouseY - height / 2);
         _vel.setMag(400);
-        player.push(new Player(player[i].pos.x, player[i].pos.y, player[i].r));
-        player[player.length - 1].pos.add(_vel);
+        player.blobs.push(new PlayerBlob(player.blobs[i].pos.x, player.blobs[i].pos.y, player.blobs[i].r));
+        player.blobs[player.blobs.length - 1].pos.add(_vel);
       }
     }
+  }
+}
+
+class Player {
+  constructor(){
+    this.blobs = [];
+    this.blobs.push(new PlayerBlob(50, 50, 20));
   }
 }
 // ------------------------------------------------------------------------------
 // ------------------------------------------------------------------------------
 function setup() {
   createCanvas(1080, 720);
-  player.push(new Player(50, 50, 20));
+  player = new Player();
 
   for (var i = 0; i < 100; i++) {
     food[i] = new Blob(random(-width, width), random(-height, height), 10);
@@ -98,20 +105,20 @@ function draw() {
 
   background(0);
   var radius = 0;
-  for (var i = 0; i < player.length; i++) {
-    radius += player[i].calculateRadius();
+  for (var i = 0; i < player.blobs.length; i++) {
+    radius += player.blobs[i].calculateRadius();
   }
 
   translate(width / 2, height / 2);
   var newzoom = 64 / radius;
   zoom = lerp(zoom, newzoom, 0.1);
   scale(zoom);
-  translate(-player[0].pos.x, -player[0].pos.y);
+  translate(-player.blobs[0].pos.x, -player.blobs[0].pos.y);
 
-  for (var i = 0; i < player.length; i++) {
-    player[i].show();
-    player[i].updatePos();
-    player[i].checkWallColision();
+  for (var i = 0; i < player.blobs.length; i++) {
+    player.blobs[i].show();
+    player.blobs[i].updatePos();
+    player.blobs[i].checkWallColision();
   }
 
   push();
@@ -121,12 +128,12 @@ function draw() {
   rect(-width, -height, width * 2, height * 2);
   pop();
 
-  // console.log(player.pos);
+  // console.log(player.blobs.pos);
 
   for (var i = 0; i < food.length; i++) {
     food[i].show();
-    for (var j = 0; j < player.length; j++) {
-      if (player[j].eats(food[i])) {
+    for (var j = 0; j < player.blobs.length; j++) {
+      if (player.blobs[j].eats(food[i])) {
         food.splice(i, 1);
         food.push(new Blob(random(-width, width), random(-height, height), 10));
       }
@@ -136,11 +143,11 @@ function draw() {
 
 function keyPressed() {
   if (keyCode == 32) {
-    Player.split();
+    PlayerBlob.split();
   }
 
   if (keyCode == 40) {
-    player[0].combine();
+    player.blobs[0].combine();
   }
 }
 // console.log(keyCode);
@@ -151,9 +158,7 @@ let animal = {
 let rabbit = {
   jumps: true
 };
-
 rabbit.__proto__ = animal; // (*)
-
 // we can find both properties in rabbit now:
 alert( rabbit.eats ); // true (**)
 alert( rabbit.jumps ); // true
